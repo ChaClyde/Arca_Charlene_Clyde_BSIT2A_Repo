3358ca30e4ca9468f6a1945bfa01e66bf3fbc1b3
unknown <carca@filamer.edu.ph>
2025-03-24 01:47:57 -0700

    added Activities

---
+++ Activites Function-activity/script.js
@@ -0,0 +1,22 @@
+
+function whatShallWear(temp) {
+    if (temp < 60) {
+        console.log("Wear a jacket");
+    }
+
+    else if (temp < 70) {
+        console.log("Wear a tshirt");
+    }
+
+    else {
+        console.log("Wear a sweater");
+    }
+       
+    }
+        
+    
+        
+whatShallWear(50)
+whatShallWear(80)
+whatShallWear(60)    
+
